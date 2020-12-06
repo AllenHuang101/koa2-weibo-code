0a563273297dc37ecb37d69d23dfb9ae23ec7d2a
allen3_huang <allen3_huang@asus.com>
2020-12-06 11:41:31 +0800

    feat: 增加 session 配置

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
+
+const { REDIS_CONF } = require('./conf/db')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -24,6 +28,21 @@ app.use(views(__dirname + '/views', {
   extension: 'ejs'
 }))
 
+// session 配置
+app.keys = ['UIsdf_7878#$']
+app.use(session({
+  key: 'weibo.sid', // cookie name 默認是 koa2.sid
+  prefix: 'weibo:sess:', // redis key 的前綴, 默認是 koa:sess:
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000 // ms
+  },
+  // ttl: 24 * 60 * 60 * 1000,
+  store: redisStore({
+    all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
+  }) 
+}))
 // // logger
 // app.use(async (ctx, next) => {
 //   const start = new Date()
